df640cb44342536ec3cfc2bc00cda830301cb448
Alex Fomushkin <aleksefo@gmail.com>
2017-06-05 22:02:13 +0300

    Fixed wrong drag force positioning and small tweaks

--- Assets/MouseManager.cs
+++ Assets/MouseManager.cs
@@ -15,11 +15,12 @@ public class MouseManager : MonoBehaviour {
             Vector2 dir = Vector2.zero;
 
             RaycastHit2D hitTest = Physics2D.Raycast(mousePos2D, dir);
-            if (hitTest && hitTest.collider != null) {
+            if (hitTest != null && hitTest.collider != null) {
                 //clicked on something with collider
                 if (hitTest.collider.GetComponent<Rigidbody2D>() != null) {
                     grabbedObject = hitTest.collider.GetComponent<Rigidbody2D>();
                     grabbedObject.gravityScale = 0;
+                    dragLine.enabled = true;
                 }
             }
         }
@@ -27,6 +28,7 @@ public class MouseManager : MonoBehaviour {
         if (Input.GetMouseButtonUp(0) && grabbedObject != null) {
             grabbedObject.gravityScale = 1;
             grabbedObject = null;
+            dragLine.enabled = false;
         }
     }
 
@@ -39,13 +41,19 @@ public class MouseManager : MonoBehaviour {
             Vector2 dir = mousePos2D - grabbedObject.position;
 
             dir *= dragSpeed;
+            
+            grabbedObject.velocity = dir;
 
-            grabbedObject.position = mousePos2D;
-            //grabbedObject.position = dir;
 
+
+        }
+    }
+    void LateUpdate() {
+        if (grabbedObject != null) {
+            Vector3 mouseWorldPos3D = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mouseWorldPos3D.x, mouseWorldPos3D.y);
             dragLine.SetPosition(0, new Vector3(grabbedObject.position.x, grabbedObject.position.y, -1));
             dragLine.SetPosition(1, new Vector3(mousePos2D.x, mousePos2D.y, 0));
-
         }
     }
 }
