a39e7b8fb87d3ba38df0eb916d2743f0cb2968fa
Alex Fomushkin <aleksefo@gmail.com>
2017-06-05 21:44:01 +0300

    Drag improved, added drag helper

--- Assets/MouseManager.cs
+++ Assets/MouseManager.cs
@@ -3,25 +3,29 @@ using System.Collections;
 
 public class MouseManager : MonoBehaviour {
 
+    public LineRenderer dragLine;
+    float dragSpeed = 4f;
     Rigidbody2D grabbedObject = null;
 
     void Update() {
         if (Input.GetMouseButtonDown(0)) {
             //Clicked
-            Vector3 MouseWorldPos3D = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 MousePos2D = new Vector2(MouseWorldPos3D.x, MouseWorldPos3D.y);
-            Vector2 Direction = Vector2.zero;
+            Vector3 mouseWorldPos3D = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mouseWorldPos3D.x, mouseWorldPos3D.y);
+            Vector2 dir = Vector2.zero;
 
-            RaycastHit2D HitTest = Physics2D.Raycast(MousePos2D, Direction);
-            if (HitTest && HitTest.collider != null) {
+            RaycastHit2D hitTest = Physics2D.Raycast(mousePos2D, dir);
+            if (hitTest && hitTest.collider != null) {
                 //clicked on something with collider
-                if (HitTest.collider.GetComponent<Rigidbody2D>() != null) {
-                    grabbedObject = HitTest.collider.GetComponent<Rigidbody2D>();
+                if (hitTest.collider.GetComponent<Rigidbody2D>() != null) {
+                    grabbedObject = hitTest.collider.GetComponent<Rigidbody2D>();
+                    grabbedObject.gravityScale = 0;
                 }
             }
         }
-
-        if (Input.GetMouseButtonUp(0)) {
+        //if grabbed something
+        if (Input.GetMouseButtonUp(0) && grabbedObject != null) {
+            grabbedObject.gravityScale = 1;
             grabbedObject = null;
         }
     }
@@ -29,10 +33,19 @@ public class MouseManager : MonoBehaviour {
     void FixedUpdate() {
         if (grabbedObject != null) {
             //moves object with mouse
-            Vector3 MouseWorldPos3D = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 MousePos2D = new Vector2(MouseWorldPos3D.x, MouseWorldPos3D.y);
+            Vector3 mouseWorldPos3D = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mouseWorldPos3D.x, mouseWorldPos3D.y);
 
-            grabbedObject.position = MousePos2D;
+            Vector2 dir = mousePos2D - grabbedObject.position;
+
+            dir *= dragSpeed;
+
+            grabbedObject.position = mousePos2D;
+            //grabbedObject.position = dir;
+
+            dragLine.SetPosition(0, new Vector3(grabbedObject.position.x, grabbedObject.position.y, -1));
+            dragLine.SetPosition(1, new Vector3(mousePos2D.x, mousePos2D.y, 0));
+
         }
     }
 }
